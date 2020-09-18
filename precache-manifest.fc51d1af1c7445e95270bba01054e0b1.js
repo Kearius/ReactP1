28d0ecfb4c5b13dede5475ae77d81283df018b15
marcin <marcin.mggm@gmail.com>
2020-09-18 19:19:39 +0200

    Updates

---
+++ precache-manifest.fc51d1af1c7445e95270bba01054e0b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4645be567304df1ab812b70a0178966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad2322e6580b2ffb935e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e63e4cee8347e2e81be9",
+    "url": "/static/js/2.6cf6baa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6cf6baa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad2322e6580b2ffb935e",
+    "url": "/static/js/main.7245e347.chunk.js"
+  },
+  {
+    "revision": "8ed0584bb3f04218bbd7",
+    "url": "/static/js/runtime-main.6fa62187.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
